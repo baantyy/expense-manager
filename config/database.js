22f2e9c9c2463b26076b51cf5fd55ee86f347636
baantyy <biswajeetbanty@gmail.com>
2019-04-27 16:54:07 +0530

    spent limit added

---
+++ config/database.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose")
+mongoose.Promise = global.Promise
+//mongodb://localhost:27017/expense-manager
+//mongodb://banty:qwe123@ds145486.mlab.com:45486/heroku_x4jcrr0w
+mongoose.connect("mongodb://banty:qwe123@ds145486.mlab.com:45486/heroku_x4jcrr0w",{
+            useNewUrlParser: true,
+            useCreateIndex: true
+        })
+        .then(function(){
+            console.log("DB is connected")
+        })
+        .catch(function(){
+            console.log("DB is not connected")
+        })
+        
+module.exports = {
+    mongoose
+}
